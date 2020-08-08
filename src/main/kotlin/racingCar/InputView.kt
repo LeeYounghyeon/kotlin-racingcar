71bf8251a79d0e94982a823eacdc3d6af1f2ec05
LeeYounghyeon <leey9494@icloud.com>
2020-08-08 13:54:14 +0900

    feat(racingCar): 자동차 이름을 입력하는 기능 구현

--- src/main/kotlin/racingCar/InputView.kt
+++ src/main/kotlin/racingCar/InputView.kt
@@ -3,18 +3,16 @@ package racingCar
 object InputView {
     private val NUMBER_REGEX = Regex("\\d+")
 
-    fun inputAmountOfPlayer(): Int {
-        println("자동차 대수는 몇 대인가요?")
-        val number = readLine()!!
-        validateInteger(number)
-        return number.toInt()
-    }
-
     fun inputAmountOfRound(): Int {
         println("시도할 횟수는 몇 회인가요?")
         val number = readLine()!!
         validateInteger(number)
         return number.toInt()
+    }
+
+    fun inputCarNames(): String {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        return readLine().orEmpty()
     }
 
     private fun validateInteger(number: String) {
