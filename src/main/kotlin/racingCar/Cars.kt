fe94fadf67eb198c4665a788820bdeefec237640
LeeYounghyeon <leey9494@icloud.com>
2020-08-08 15:38:31 +0900

    feat(racingCar): 우승자 목록을 구하는 기능 및 테스트 구현

--- src/main/kotlin/racingCar/Cars.kt
+++ src/main/kotlin/racingCar/Cars.kt
@@ -2,8 +2,8 @@ package racingCar
 
 import kotlin.random.Random
 
-class Cars(numberOfPlayer: Int, carNames: List<String>) {
-    val cars = (0 until numberOfPlayer).map { Car(carNames[it]) }
+class Cars(carNames: List<String>) {
+    val cars = (carNames.indices).map { Car(carNames[it]) }
 
     fun moveCars() {
         cars.forEach { it.move(getRandomNumbers()) }
